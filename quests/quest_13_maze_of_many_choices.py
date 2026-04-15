3e3867ecd8ad23fe7c0e2cac5b253fcd92f4f530
Kalungibrightangel <b.kalungi@alustudent.com>
2026-04-15 22:54:19 +0400

    Using if-elif-else statement - This lets you check multiple different conditions in a row.

---
+++ quests/quest_13_maze_of_many_choices.py
@@ -0,0 +1,9 @@
+score = int(input("Enter the score: "))
+if int(score) >= 90:
+    print("A")
+elif int(score) >= 80:
+    print("B")
+elif int(score) >= 70:
+    print("C")
+else:
+    print("Needs Improvement")
