f54446a0050559c13060a39bb1dffdbd5053335f
IamAMONDI <e.otieno1@alustudent.com>
2026-04-15 23:53:55 +0400

    correct file

--- quests/quest_24_master_spell.py
+++ quests/quest_24_master_spell.py
@@ -1,16 +1,15 @@
-
-def ask_for_age ():
-    age = int(input("How old are you?"))
-    return age 
-
-def can_they_vote ():
-     age = ask_for_age()
-         if ask_for_age() >= 18 
-              print ("You are allowed to vote")
-            else 
-                 print ("Wait a few more years")
-
-     
-can_they_vote
+def ask_for_age():
+    age = int(input("How old are you? "))
+    return age
 
 
+def can_they_vote():
+    age = ask_for_age()
+
+    if age >= 18:
+        print("You are allowed to vote")
+    else:
+        print("Wait a few more years")
+
+
+can_they_vote()
