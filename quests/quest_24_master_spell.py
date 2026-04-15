1027ee8c0c43ed7ecdb16b78373dc35661a93792
IamAMONDI <e.otieno1@alustudent.com>
2026-04-15 23:47:19 +0400

    no 24 a function inside another

---
+++ quests/quest_24_master_spell.py
@@ -0,0 +1,16 @@
+
+def ask_for_age ():
+    age = int(input("How old are you?"))
+    return age 
+
+def can_they_vote ():
+     age = ask_for_age()
+         if ask_for_age() >= 18 
+              print ("You are allowed to vote")
+            else 
+                 print ("Wait a few more years")
+
+     
+can_they_vote
+
+
