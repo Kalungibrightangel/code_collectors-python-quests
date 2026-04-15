31620381c79440b5739c95871b9d86b18569738d
IamAMONDI <e.otieno1@alustudent.com>
2026-04-15 22:41:58 +0400

    did division and modulo to share

---
+++ quests/quest_09_greedy_goblin.py
@@ -0,0 +1,7 @@
+#this code does divison using the modulo data type
+
+pieces = 27//4
+
+remainder = 27 % 4
+print ( "Each goblin will get " + str(pieces) ) 
+print ( "The greedy goblin will remain with " + str(remainder) )
