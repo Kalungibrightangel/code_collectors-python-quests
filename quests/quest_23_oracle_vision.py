b5785c12be4f954a74f806a45523009c3c800830
Kalungibrightangel <b.kalungi@alustudent.com>
2026-04-16 00:34:30 +0400

    Functions that return a value.

---
+++ quests/quest_23_oracle_vision.py
@@ -0,0 +1,12 @@
+def calculate_area(length, width):
+    return length * width
+
+length1 = 25.8
+width1 = 10
+
+length2 = 90
+width2 = 20
+
+print("Area of first rectangle: ",calculate_area(length1, width1))
+
+print("Area of second rectangle: ", calculate_area (length2, width2))
