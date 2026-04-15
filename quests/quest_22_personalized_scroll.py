9648a6310cb1e3a691cfe647dd30407326b7b83e
paokyereagy <p.okyereagy@alustudent.com>
2026-04-15 23:26:03 +0400

    working on functions with arguments

---
+++ quests/quest_22_personalized_scroll.py
@@ -0,0 +1,10 @@
+def personalized_greeting(name, quest):
+     print("Hello, " + name + "Your quest is " + quest ) 
+
+
+
+
+user_name= input("Enter your name: ") 
+quest_name= input("Enter the name of your quest: ") 
+personalized_greeting(user_name, quest_name) 
+  
