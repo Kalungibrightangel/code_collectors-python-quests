e377f66ba08ffea6ccd5f4789c4fe206f4e5f225
Mizeroo <l.mizero@alustudent.com>
2026-04-16 10:57:44 +0400

    if condition inside while loop

--- quests/quest_25_number _wizard.py
+++ quests/quest_25_number _wizard.py
@@ -1,11 +1,12 @@
+#updating number guessing game (quest 18) by adding if condition inside while loop
 secret_number = 78
 user_input = int(input("enter any number you can think of: "))
 
 while int(user_input) != int(secret_number):
     if int(user_input) < int(secret_number):
-        print("Oops! your guess was too low")
+        print("Oaps! Your guess was too low")
     if int(user_input) > int(secret_number):
-        print("Bingo! your guess was too high")
-    user_input = int(input("Ooh, You got it wrong try again: "))
+        print("Your guess is  too high")
+    user_input = int(input("try again: "))
     
 print("Wow, you got it right, congratulations")
